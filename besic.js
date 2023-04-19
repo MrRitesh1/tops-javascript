5947c057b1ed926cc27ed4b4c67b884399c21598
Ritesh Navadiya <riteshnavadiya111@gmail.com>
2023-04-19 11:13:00 +0530

    19/04/2023 Prectiss for javascript basic ...

---
+++ besic.js
@@ -0,0 +1,13 @@
+var a; // undefined
+let b; // undefined
+// const c ; //'const' declarations must be initialized.
+console.log("var = ", a);
+console.log("let = ", b);
+
+var w = 10e10;
+console.log("0e0 = ", w);
+var q = 9e-3;
+console.log("0e-0 = ", q);
+
+console.log(10 + 20 + "Welcome");
+console.log("Welcome" + 10 + 20);
---
+++ object.js
@@ -0,0 +1,12 @@
+const itam = {
+  name: "Ritesh",
+  addres: "surat",
+  numbar: "1234567890",
+  email: "riteshnavadiya111@gmail.com",
+};
+
+console.log(itam.name);
+console.log("-----------------------------------");
+// itam object in values add .....
+itam["Friend"] = "Harsh";
+console.log(itam);
